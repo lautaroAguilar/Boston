c5529b3837b8619c45f2367041a3ee8f6d283b47
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2025-01-17 12:42:31 -0300

    get off createApp() and inject dependency

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,19 @@
 import express, { json } from 'express'
-import { createCompanyRouter } from './routes/company.js'
 import corsMiddleware from './middlewares/cors.js'
 import 'dotenv/config'
+import { companiesRouter } from './routes/company/company.js'
 
-export const createApp = ({ companyModel }) => {
-  const app = express()
-  const PORT = process.env.PORT ?? 3000
-  app.use(json())
-  app.use(corsMiddleware())
-  app.disable('x-powered-by')
-  
-  app.use('/company', createCompanyRouter({ companyModel }))
-  
-  app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`)
-  })
-}
+const app = express()
+const PORT = process.env.PORT ?? 3000
+app.use(json())
+app.use(corsMiddleware())
+app.disable('x-powered-by')
+
+app.use('/api/companies', companiesRouter)
+/* app.use('/api/user', Router)
+  app.use('/api/auth', Router)
+  app.use('/api/course', Router)
+  app.use('/api/education', Router) */
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`)
+})
