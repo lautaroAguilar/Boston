f00129aa63123fbbf36f695c7a59878a8f3a5add
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2025-02-10 11:42:02 -0300

    adding Stepper in Form component and implementing it in the empresas page

--- frontend/src/app/(panel)/empresas/page.jsx
+++ frontend/src/app/(panel)/empresas/page.jsx
@@ -1,97 +1,154 @@
 "use client";
-import React, { useEffect } from "react";
-import { Stack, Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Stack, Button, Paper, Modal, Typography } from "@mui/material";
 import { useCompany } from "@/contexts/companies";
 import { DataGrid } from "@mui/x-data-grid";
-import { ArrowDropDown, ArrowDropUp } from "@mui/icons-material";
+import {
+  ApartmentRounded,
+  ArrowDropDown,
+  ArrowDropUp,
+} from "@mui/icons-material";
+import { useDashboard } from "@/contexts/dashboard";
+import MyForm from "@/components/Form";
+function ExpandableCell({ value }) {
+  const [expanded, setExpanded] = React.useState(false);
+  return (
+    <Stack flexDirection={"row"} gap={1} alignItems={"center"}>
+      {value && value.length > 10 && (
+        <Button
+          sx={{ height: 30, padding: 0, margin: 0, minWidth: 30 }}
+          onClick={() => setExpanded(!expanded)}
+        >
+          {expanded ? <ArrowDropUp /> : <ArrowDropDown />}
+        </Button>
+      )}
+      {expanded
+        ? value
+        : value && value.length > 10
+          ? `${value.slice(0, 10)}...`
+          : value}
+      &nbsp;
+    </Stack>
+  );
+}
+const columns = [
+  {
+    field: "id",
+    headerName: "ID",
+    width: 150,
+    renderCell: (params) => <ExpandableCell {...params} />,
+  },
+  { field: "name", headerName: "Nombre", width: 150 },
+  { field: "SID", headerName: "SID", width: 150 },
+  { field: "business_name", headerName: "Razón social", width: 150 },
+  {
+    field: "costCenters",
+    headerName: "Centros de costo",
+    flex: 1,
+    renderCell: (params) => {
+      const value =
+        params.value?.success === true && params.value?.data?.length === 0
+          ? params.value.message
+          : params.value?.map((cc) => `- ${cc.cost_center_name}`).join("\n");
+
+      return <ExpandableCell value={value} />;
+    },
+  },
+  {
+    field: "sectors",
+    headerName: "Sectores",
+    flex: 1,
+    renderCell: (params) => {
+      const value =
+        params.value?.success === true && params.value?.data?.length === 0
+          ? params.value.message
+          : params.value?.map((s) => `- ${s.sector_name}`).join("\n");
+      return <ExpandableCell value={value} />;
+    },
+  },
+  {
+    field: "contacts",
+    headerName: "Contactos",
+    flex: 1,
+    renderCell: (params) => {
+      const value =
+        params.value?.success === true && params.value?.data?.length === 0
+          ? params.value.message
+          : params.value
+              ?.map(
+                (c) =>
+                  `- ${c.contact_name} (${c.contact_email}) ${c.contact_notes}`
+              )
+              .join("\n");
+      return <ExpandableCell value={value} />;
+    },
+  },
+];
 export default function Page() {
   const { fetchCompaniesInfo, companies } = useCompany();
-  /* COMPONENTE PARA EXPANDIR CELDA CUANDO HAY MUCHA INFORMACIÓN */
-  function ExpandableCell({ value }) {
-    const [expanded, setExpanded] = React.useState(false);
-
-    return (
-      <Stack flexDirection={"row"} gap={1} alignItems={"center"}>
-        {value && value.length > 10 && (
-          <Button
-            sx={{ height: 30, padding: 0, margin: 0, minWidth: 30 }}
-            onClick={() => setExpanded(!expanded)}
-          >
-            {expanded ? <ArrowDropUp /> : <ArrowDropDown />}
-          </Button>
-        )}
-        {expanded
-          ? value
-          : value && value.length > 10
-            ? `${value.slice(0, 10)}...`
-            : value}
-        &nbsp;
-      </Stack>
-    );
-  }
-  const columns = [
-    {
-      field: "id",
-      headerName: "ID",
-      width: 150,
-      renderCell: (params) => <ExpandableCell {...params} />,
-    },
-    { field: "name", headerName: "Nombre", width: 150 },
-    { field: "SID", headerName: "SID", width: 150 },
-    { field: "business_name", headerName: "Razón social", width: 150 },
-    {
-      field: "costCenters",
-      headerName: "Centros de costo",
-      flex: 1,
-      renderCell: (params) => {
-        const value =
-          params.value?.success === true && params.value?.data?.length === 0
-            ? params.value.message
-            : params.value?.map((cc) => `- ${cc.cost_center_name}`).join("\n");
-
-        return <ExpandableCell value={value} />;
+  const { setToolbarButtonAction } = useDashboard();
+  const [showForm, setShowForm] = useState(false);
+  const [formCompanyValues, setFormCompanyValues] = useState({
+    name: "",
+    cuit: "",
+    business_name: "",
+    sid: "",
+    survey_link: "",
+    cost_center: "",
+  });
+  const fieldsCompany = [
+    [
+      { name: "name", label: "Nombre", required: true },
+      { name: "cuit", label: "CUIT", required: true },
+      { name: "business_name", label: "Razón Social", required: true },
+      { name: "sid", label: "SID" },
+      { name: "survey_link", label: "Link de encuesta" },
+    ],
+    [
+      {
+        name: "cost_center",
+        label: "Centro de costo",
+        type: "text",
+        required: true,
       },
-    },
-    {
-      field: "sectors",
-      headerName: "Sectores",
-      flex: 1,
-      renderCell: (params) => {
-        const value =
-          params.value?.success === true && params.value?.data?.length === 0
-            ? params.value.message
-            : params.value?.map((s) => `- ${s.sector_name}`).join("\n");
-        return <ExpandableCell value={value} />;
-      },
-    },
-    {
-      field: "contacts",
-      headerName: "Contactos",
-      flex: 1,
-      renderCell: (params) => {
-        const value =
-          params.value?.success === true && params.value?.data?.length === 0
-            ? params.value.message
-            : params.value
-                ?.map(
-                  (c) =>
-                    `- ${c.contact_name} (${c.contact_email}) ${c.contact_notes}`
-                )
-                .join("\n");
-        return <ExpandableCell value={value} />;
-      },
-    },
+    ],
   ];
-
+  const handleChangeCompany = (fieldName, newValue) => {
+    setFormCompanyValues((prev) => ({
+      ...prev,
+      [fieldName]: newValue,
+    }));
+  };
+  function handleShowForm() {
+    setShowForm(true);
+  }
+  function handleSubmitStepOne() {
+    console.log(formCompanyValues, "step one");
+    // Aquí puedes agregar lógica para validar los datos
+    // Si todo está bien, devuelve true
+    return true; // Asegúrate de que esto se ejecute si todo está correcto
+  }
+  function handleSubmitStepTwo() {
+    console.log(formCompanyValues, "step two");
+    return true;
+  }
   /* LLAMAMOS A LAS EMPRESAS CON SU INFO + CC, SECTORES Y CONTACTOS */
   useEffect(() => {
     fetchCompaniesInfo();
-  }, []);
+    setToolbarButtonAction({
+      label: "Crear nueva",
+      action: handleShowForm,
+      icon: <ApartmentRounded />,
+    });
+  }, [setToolbarButtonAction]);
   return (
     <Stack
       sx={{
         height: "100%",
         width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
       <DataGrid
@@ -101,6 +158,7 @@ export default function Page() {
         getRowHeight={() => "auto"}
         /* slots={{ toolbar: GridToolbar }} */
         sx={{
+          width: "100%",
           "&.MuiDataGrid-root--densityCompact .MuiDataGrid-cell": {
             py: 1,
           },
@@ -112,6 +170,41 @@ export default function Page() {
           },
         }}
       />
+      <Modal open={showForm} onClose={() => setShowForm(false)} sx={{height: "100%"}}>
+        <Paper
+          elevation={4}
+          square={false}
+          sx={{
+            height: "80%",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            p: 2,
+            gap: 2,
+            maxWidth: 500,
+            maxHeight: 600,
+            overflowY: "auto",
+            scrollbarWidth: "thin",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <Typography variant="h5">Crear nueva empresa</Typography>
+          <MyForm
+            steps={["Paso 1", "Paso 2"]}
+            fields={fieldsCompany}
+            values={formCompanyValues}
+            onChange={handleChangeCompany}
+            handleSubmitPerStep={{
+              "Paso 1": handleSubmitStepOne,
+              "Paso 2": handleSubmitStepTwo,
+            }}
+            buttonText={"Crear empresa"}
+          />
+        </Paper>
+      </Modal>
     </Stack>
   );
 }
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -10,25 +10,84 @@ import {
   FormControl,
   FormHelperText,
   Typography,
+  Stepper,
+  Step,
+  StepLabel,
+  useMediaQuery,
 } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-/* import MyInput from "./input"; */
+
 export default function MyForm({
   fields,
   values = {},
   onChange,
-  onSubmit,
+  onSubmit, // ⬅️ Se usa si no hay steps
+  handleSubmitPerStep = {}, // ⬅️ Se usa si hay steps
   buttonText,
   errors = {},
   successMessage,
   errorMessage,
+  steps = [], // ⬅️ Si hay pasos, se usa el Stepper
 }) {
   const [showPassword, setShowPassword] = useState(false);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit(e);
+  const [activeStep, setActiveStep] = useState(0);
+  const [completedSteps, setCompletedSteps] = useState(new Set()); // Estado para steps completados
+  const isMobile = useMediaQuery("(max-width:600px)");
+  // Determinar si se usa Steps o no
+  const isStepperActive = steps.length > 0;
+
+  const handleNext = async () => {
+    if (isStepperActive) {
+      const currentStepKey = steps[activeStep];
+      const handleStep = handleSubmitPerStep[currentStepKey];
+
+      if (handleStep) {
+        if (!completedSteps.has(currentStepKey)) {
+          // Se verifica si el paso fue completado
+          const success = await handleStep();
+          if (success) {
+            // Marcar el paso como completado
+            setCompletedSteps((prev) => new Set(prev).add(currentStepKey));
+            // Solo avanza si es exitoso
+            setActiveStep((prev) => prev + 1);
+          } else {
+            console.log("no fue satisfactorio");
+          }
+        } else {
+          // Si ya estaba completado el paso, avanza solamente
+          setActiveStep((prev) => prev + 1);
+        }
+      } else {
+        // Avanza si no hay función de manejo
+        setActiveStep((prev) => prev + 1);
+      }
+    }
   };
+
+  const handleBack = () => {
+    if (isStepperActive) {
+      setActiveStep((prev) => prev - 1);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (isStepperActive) {
+      if (activeStep < steps.length - 1) {
+        handleNext();
+      } else {
+        const finalStep = handleSubmitPerStep[steps[activeStep]];
+        if (finalStep) await finalStep();
+      }
+    } else {
+      // ⬅️ Si no hay steps, ejecuta el onSubmit normal
+      await onSubmit();
+    }
+  };
+
+  // Filtrar los campos según el paso actual (si hay steps)
+  const currentFields = isStepperActive ? fields[activeStep] : fields;
 
   return (
     <Box
@@ -43,9 +102,10 @@ export default function MyForm({
         justifyContent: "space-between",
       }}
     >
-      <Stack spacing={2}>
-        {fields.map((field, index) => (
-          <Stack xs={12} sm={6} key={index}>
+      {/* Renderizar los campos del step actual o todos si no hay steps */}
+      <Stack spacing={2} sx={{ mt: 2 }}>
+        {currentFields.map((field, index) => (
+          <Stack key={index}>
             {(() => {
               switch (field.component) {
                 case "select":
@@ -74,7 +134,7 @@ export default function MyForm({
                   return (
                     <DatePicker
                       label={field.label}
-                      value={(values && values[field.name]) ?? null}
+                      value={values[field.name] ?? null}
                       onChange={(newValue) =>
                         onChange(
                           field.name,
@@ -97,7 +157,7 @@ export default function MyForm({
                       label={field.label}
                       name={field.name}
                       required={field.required ?? false}
-                      value={(values && values[field.name]) ?? ""}
+                      value={values[field.name] ?? ""}
                       onChange={(e) => onChange(field.name, e.target.value)}
                       error={Boolean(errors[field.name])}
                       helperText={errors[field.name] ?? ""}
@@ -125,9 +185,39 @@ export default function MyForm({
         <Typography>{successMessage}</Typography>
         <Typography>{errorMessage}</Typography>
       </Stack>
-      <Stack sx={{ mt: 2, textAlign: "center" }}>
+      {/* Stepper solo si hay pasos */}
+      {isStepperActive && (
+        <Stepper activeStep={activeStep} sx={{ width: "100%" }}>
+          {steps.map((label, index) => (
+            <Step key={index}>
+              <StepLabel>{!isMobile ? label : ""}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      )}
+
+      {/* Botones de navegación */}
+      <Stack
+        sx={{
+          mt: 2,
+          width: "100%",
+          display: "flex",
+          justifyContent: "flex-end",
+        }}
+        spacing={2}
+        direction="row"
+      >
+        {isStepperActive && activeStep > 0 && (
+          <Button variant="outlined" onClick={handleBack}>
+            Anterior
+          </Button>
+        )}
         <Button type="submit" variant="contained">
-          {buttonText}
+          {isStepperActive && activeStep === steps.length - 1
+            ? buttonText
+            : isStepperActive
+              ? "Siguiente"
+              : buttonText}
         </Button>
       </Stack>
     </Box>
