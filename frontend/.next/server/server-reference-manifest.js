71f83d70d3cd73a83947be8532a3622e78d8050e
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2025-03-20 11:18:50 -0300

    npm run build from local

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"AjF53LQn0XSjj2eNRVKgv2eHHfrUAUrZBOeIEoRdZow=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
