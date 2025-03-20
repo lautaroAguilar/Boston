df03df4190aee6727b325c1b0797f99425a4af17
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2025-03-20 15:52:08 -0300

    delete server.js, change config api, add .next into gitignore and run npm run build

--- frontend/config/api.js
+++ frontend/config/api.js
@@ -1,24 +1,5 @@
-const getApiBaseUrl = () => {
-  // Detectar el entorno basado en la URL actual
-  const hostname =
-    typeof window !== "undefined" ? window.location.hostname : "";
-
-  // Comprobar si estamos en el ambiente de prueba o producción
-  if (hostname.includes("sistema-test.bostoncelop.com.ar")) {
-    return {
-      API_URL: "https://api.sistema-test.bostoncelop.com.ar/api",
-    };
-  } else if (hostname.includes("sistema.bostoncelop.com.ar")) {
-    return {
-      API_URL: "https://api.sistema.bostoncelop.com.ar/api",
-    };
-  }
-
-  // Fallback para desarrollo local
-  return {
-    API_URL: "http://localhost:3000/api",
-  };
+const CONFIG = {
+  API_URL: process.env.NEXT_PUBLIC_API_URL,
 };
-const CONFIG = getApiBaseUrl();
 
 export default CONFIG;
